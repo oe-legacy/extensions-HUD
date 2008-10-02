a16d7b8304834af89cb6188d7ff162935d0efa1d
zerny <zerny@openengine.dk>
2008-10-02 15:16:43 +0200

    Created a HUD for textures.
    darcs-hash:20081002131643-50fb2-b01ee31397efc2335cdf9fa13130ff31cf2280e9.gz

---
+++ Display/HUD.cpp
@@ -0,0 +1,176 @@
+// Heads Up Display.
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+#include <Meta/OpenGL.h> // @todo: remove this, please
+
+#include <Display/HUD.h>
+#include <Math/Vector.h>
+#include <Geometry/Face.h>
+#include <Logging/Logger.h>
+
+#include <Display/Orthotope.h>
+#include <Display/ViewingVolume.h>
+
+namespace OpenEngine {
+namespace Display {
+
+using std::list;
+using OpenEngine::Math::Vector;
+using OpenEngine::Geometry::Face;
+using OpenEngine::Geometry::FacePtr;
+using OpenEngine::Logging::Logger;
+using OpenEngine::Display::Orthotope;
+using OpenEngine::Display::ViewingVolume;
+using OpenEngine::Renderers::RenderingEventArg;
+using OpenEngine::Resources::ITextureResourcePtr;
+
+HUD::HUD() {
+
+}
+
+HUD::~HUD() {
+    list<Surface*>::iterator itr;
+    for (itr=surfaces.begin(); itr != surfaces.end(); itr++)
+        delete *itr;
+    surfaces.clear();
+}
+
+/**
+ * Redraw on a rendering event.
+ * Usually you will want to attach this to the post-process event of
+ * your renderer. 
+ */
+void HUD::Handle(RenderingEventArg arg) {
+    // @todo: listen for frame changes so our HUD is correct
+    ViewingVolume* vv = new ViewingVolume();
+    Orthotope* ot = new Orthotope(*vv, -1, 1, 0, 800, 600, 0);
+    arg.renderer.ApplyViewingVolume(*ot);
+    delete ot;
+    delete vv;
+
+    // We need to disabled the depth test otherwise we will not draw
+    // things in the correct order.
+    // @todo: Do this in a general way (read: not gl specific)
+    bool depth = glIsEnabled(GL_DEPTH_TEST);
+    if (depth) glDisable(GL_DEPTH_TEST);
+
+    Vector<3,float> v1,v2,v3,v4,n;
+    Vector<2,float> t1,t2,t3,t4;
+    unsigned int x, y, w, h;
+    list<Surface*>::iterator itr;
+    for (itr=surfaces.begin(); itr != surfaces.end(); itr++) {
+        //(*itr)->texr->Load();
+        if ((*itr)->texr->GetID() == 0)
+            arg.renderer.LoadTexture((*itr)->texr);
+        //(*itr)->texr->Unload();
+
+        x = (*itr)->x;
+        y = (*itr)->y;
+        w = (*itr)->texr->GetWidth();
+        h = (*itr)->texr->GetHeight();
+
+        n  = Vector<3,float>(0,0,1);
+        v1 = Vector<3,float>(x,   y,   0);
+        v2 = Vector<3,float>(x,   y+h, 0);
+        v3 = Vector<3,float>(x+w, y+h, 0);
+        v4 = Vector<3,float>(x+w, y,   0);
+        t2 = Vector<2,float>(0, 0);
+        t1 = Vector<2,float>(0, 1);
+        t4 = Vector<2,float>(1, 1);
+        t3 = Vector<2,float>(1, 0);
+
+        FacePtr f = FacePtr(new Face(v1,v2,v3,n,n,n));
+        f->mat->texr = (*itr)->texr;
+        f->vert[0] = v1; f->vert[1] = v2; f->vert[2] = v3;
+        f->texc[0] = t1; f->texc[1] = t2; f->texc[2] = t3;
+        arg.renderer.DrawFace(f);
+        f->vert[0] = v1; f->vert[1] = v3; f->vert[2] = v4;
+        f->texc[0] = t1; f->texc[1] = t3; f->texc[2] = t4;
+        arg.renderer.DrawFace(f);
+    }
+
+    if (depth) glEnable(GL_DEPTH_TEST);
+}
+
+/**
+ * Create a new surface from a texture.
+ * To remove the surface from the HUD, simply delete it.
+ */
+HUD::Surface* HUD::CreateSurface(ITextureResourcePtr texr,
+                                 const unsigned int x,
+                                 const unsigned int y) {
+    Surface* w = new Surface(*this, texr, x, y);
+    surfaces.push_back(w);
+    // save the iterator
+    w->itr = --surfaces.end();
+    return w;
+}
+
+HUD::Surface::Surface(HUD& hud,
+                      ITextureResourcePtr texr,
+                      unsigned int x,
+                      unsigned int y)
+    : hud(hud)
+    , texr(texr)
+    , x(x)
+    , y(y) {
+    
+}
+
+/**
+ * Deleting a surface will remove it from the HUD.
+ */
+HUD::Surface::~Surface() {
+    hud.surfaces.erase(itr);
+}
+
+/**
+ * Move the surface to the front of the HUD.
+ */
+void HUD::Surface::MoveToFront() {
+    hud.surfaces.erase(itr);    // invalidation
+    hud.surfaces.push_back(this);
+    itr = --hud.surfaces.end(); // new iterator
+}
+
+/**
+ * Move the surface to the back of the HUD.
+ */
+void HUD::Surface::MoveToBack() {
+    hud.surfaces.erase(itr);      // invalidation
+    hud.surfaces.push_front(this);
+    itr = hud.surfaces.begin();   // new iterator
+}
+
+/**
+ * Set the position of the surface on screen.
+ */
+void HUD::Surface::SetPosition(const unsigned int x,
+                               const unsigned int y) {
+    this->x = x;
+    this->y = y;
+}
+
+/**
+ * Set the position of the surface on screen.
+ */
+void HUD::Surface::SetPosition(const Vector<2,int> xy) {
+    x = xy.Get(0);
+    y = xy.Get(1);
+}
+
+/**
+ * Get the position of the surface on screen.
+ */
+Vector<2,int> HUD::Surface::GetPosition() const {
+    return Vector<2,int>(x,y);
+}
+
+} // NS Display
+} // NS OpenEngine
---
+++ Display/HUD.h
@@ -0,0 +1,73 @@
+// Heads up display.
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+#ifndef _OE_HUD_H_
+#define _OE_HUD_H_
+
+#include <Core/IListener.h>
+#include <Renderers/IRenderer.h>
+#include <Resources/ITextureResource.h>
+#include <Math/Vector.h>
+#include <list>
+
+namespace OpenEngine {
+namespace Display {
+
+/**
+ * Heads Up Display.
+ *
+ * @class HUD HUD.h Display/HUD.h
+ */
+class HUD : public Core::IListener<Renderers::RenderingEventArg> {
+public:
+
+    /**
+     * Surface in the HUD.
+     *
+     * Can only be created by the HUD.
+     * Deleting a surface will automatically remove it from the HUD.
+     *
+     * @see HUD::CreateSurface
+     */
+    class Surface {
+    public:
+        virtual ~Surface();
+        void MoveToFront();
+        void MoveToBack();
+        void SetPosition(const unsigned int x, const unsigned int y);
+        void SetPosition(const Math::Vector<2,int> xy);
+        Math::Vector<2,int> GetPosition() const;
+    protected:
+        friend class HUD;
+        HUD& hud;
+        std::list<Surface*>::iterator itr;
+        Resources::ITextureResourcePtr texr;
+        unsigned int x, y;
+        Surface(HUD&,
+                Resources::ITextureResourcePtr,
+                unsigned int x,
+                unsigned int y);
+    };
+
+    HUD();
+    virtual ~HUD();
+    virtual void Handle(Renderers::RenderingEventArg arg);
+    virtual Surface* CreateSurface(Resources::ITextureResourcePtr texr,
+                                   const unsigned int x=0,
+                                   const unsigned int y=0);
+
+protected:
+    std::list<Surface*> surfaces;
+
+};
+
+} // NS Display
+} // NS OpenEngine
+
+#endif // _OE_HUD_H_
